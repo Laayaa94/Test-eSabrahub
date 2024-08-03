030edf61b759cc1f44e5b8e13ea2731040828c9b
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-04 01:26:14 +0530

    Frontend all services pages

---
+++ frontend/src/Components/ServicesCompo/MedicalCompo.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const MedicalCompo = () => {
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/service?postType=medical');
+        setServices(response.data);
+      } catch (error) {
+        setError('Failed to fetch services. Please try again.');
+        console.error('Error fetching accommodation services:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Accommodation Services</h1>
+      {services.length > 0 ? (
+        <div className="services-list">
+          {services.map((service) => (
+            <div key={service._id} className="service-item">
+              <img
+                src={`http://localhost:5000/uploads/mainphotos/${service.mainPhoto}`}
+                alt={service.name}
+                className="service-main-photo"
+              />
+              <h2>{service.name}</h2>
+              <p>Location: {service.location}</p>
+              
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No accommodation services available.</p>
+      )}
+    </div>
+  );
+};
+
+export default MedicalCompo;
