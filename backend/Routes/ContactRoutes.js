a12cac1e36b6432e39d1f78590839424049fc1cf
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-07 18:29:22 +0530

    contact us backend code

--- backend/Routes/ContactRoutes.js
+++ backend/Routes/ContactRoutes.js
@@ -1,33 +1,9 @@
+// routes/contactUsRoutes.js
+
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer');
-require('dotenv').config(); // Ensure environment variables are loaded
+const contactUsController = require('../Controllers/ContactUsController');
 
-// Configure Nodemailer directly in the route file
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
-router.post('/send', (req, res) => {
-  const { fullName, email, message } = req.body;
-
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_USER,
-    subject: 'Contact Us Form Submission',
-    text: `Full Name: ${fullName}\nEmail: ${email}\nMessage: ${message}`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).send(error.toString());
-    }
-    res.status(200).send('Message sent: ' + info.response);
-  });
-});
+router.post('/msgsent', contactUsController.createMessage);
 
 module.exports = router;
